962389def61c63157313e478d90721d68d77bd8e
Valentin Horacek <valentin.horacek@gmail.com>
2025-04-24 16:12:23 +0200

    Developer page - initial setup

---
+++ src/Portfolio/Portfolio_Server/Models/SkillItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Portfolio_Server.Models
+{
+    public class SkillItem
+    {
+        public string Title { get; set; }
+        public List<string> Skills { get; set; }
+    }
+}
---
+++ src/Portfolio/Portfolio_Server/Models/TimelineItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Portfolio_Server.Models
+{
+    public class TimelineItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+    }
+}
